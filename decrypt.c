1a3cb108e953801c9f6bdca521f07b75ddd04c86
Koppelberger <koppelbw@eos18.cis.gvsu.edu>
2013-02-12 20:39:07 -0500

    4th - calcFreq Complete

--- decrypt.c
+++ decrypt.c
@@ -15,24 +15,59 @@ void readFreq(float given[]){
 	char buf[1024];
 	int cnt = 1, i = 0;
 	while((fgets(buf,1024,ifp)) != NULL){
-		//buf[strlen(buf)-1] = 0;
 		char *tok;
 		tok = strtok(buf, "\n ");
-		//cnt++;
-		//if(!(cnt%2)){
 		tok = strtok(NULL, "\n ");
 		given[i] = atof(tok);
 		i++;
-		//}
-		//cnt++;
 	}
 }
 
 
+void calcFreq(float found[]){
+
+	FILE *ifp;
+	ifp = fopen("data.out", "r");
+	
+	if(ifp == NULL){
+		printf("Error: Could not open encrypted file data.out!");
+		exit(0);
+	}
+	
+	int charCount = 0;
+
+	int i;
+	for(i = 0; i < ALPH; i++)
+		found[i] = 0;
+	char tmp = fgetc(ifp);
+	while(tmp != EOF){
+		if(isalpha(tmp)){
+			charCount++;
+			tmp = toupper(tmp);
+			found[tmp - 'A']++;
+		}
+		tmp = fgetc(ifp);
+		
+	}
+
+	//for loop to divide by the total number of chars, to get the ratio of each letter
+	for(i = 0; i < ALPH; i++){
+		found[i] /= charCount;
+	}
+}
+
 int main(void){
 
-	float given[ALPH];
+	float given[ALPH], found[ALPH];
+	
+
 	readFreq(given);
+	calcFreq(found);
+	
+	int i;
+	for(i = 0; i < ALPH; i++){
+		printf("%d: %f\n", i, found[i]);
+	}
 
 	return 0;
 }
